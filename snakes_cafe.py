efdcd651c882a89ffcbb3a167261520aa9664107
Liesl White <lieslfwhite@gmail.com>
2022-07-19 19:35:00 -0700

    lab 01 complete

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -36,22 +36,34 @@ Unicorn Tears
 ***********************************
 """)
 
-# initialize empty meal dictionary
+# EMPTY MEAL DICTIONARY
+menu_items = {
+    "Wings": 0, "Cookies": 0, "Spring Rolls": 0,
+    "Salmon": 0, "Steak": 0, "Meat Tornado": 0, "A Literal Garden": 0,
+    "Ice Cream": 0, "Cake": 0, "Pie": 0,
+    "Coffee": 0, "Tea": 0, "Unicorn Tears": 0
+}
 
-# start loop here until user enters quit
+# VARIABLES
+# sing_response = f"** {num_items} order of {order} has been added to your meal **"
+# plur_response = f"** {num_items} orders of {order} have been added to your meal **"
+customer_ticket = {}
 
+# start loop and repeat until user enters 'quit'
 
-# create variable to store user response
-order = input('> ')
+while True:
+    order = input('> ').capitalize()
+    if order == "Quit":
+        break
+    elif order not in menu_items:
+        print('**Sorry! We don\'t seem to carry that. Would you like to order anything else?**')
+        continue
+    elif order not in customer_ticket:
+        customer_ticket.update({order: 0})
+    customer_ticket[order] += 1
+    print(f'** {customer_ticket[order]} order of {order} have been added to your meal.**')
+    # else:
+    # print(f'** {customer_ticket[order]} orders of {order} have been added to your meal.**')
 
-# print out response of order
-num_items = 1 # TODO properly tally items that haven't been ordered
-response = f"** {num_items} order of {order} have been added to your meal **"
+# end/break loop when user quits
 
-print(response)
-
-order = input('> ')
-
-print(order)
-
-# end loop when user quits
